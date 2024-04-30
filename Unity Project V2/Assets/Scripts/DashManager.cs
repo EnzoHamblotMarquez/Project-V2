db9572e6d94b5a2304a81efb51e540237078616c
EnzoHamblotMarquez <ehamblotm@edu.tecnocampus.cat>
2024-04-30 15:36:52 +0200

    CharacterController and DashManager

---
+++ Unity Project V2/Assets/Scripts/DashManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DashManager : MonoBehaviour
+{
+    [SerializeField] private float dashSpeedBoostPercentage;
+    private float dashSpeed;
+    [SerializeField] private float dashDuration;
+
+    [SerializeField] private float dashTotalCooldown; //May need it to be higher than dashDuration !!
+    private float dashCurrentCooldown;
+    private bool dashInCooldown;
+
+    private PlayerController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+
+        dashSpeed = playerController.GetBaseSpeed() * (1 + dashSpeedBoostPercentage / 100); 
+
+        dashInCooldown = false;
+    }
+
+    // Update is called once per frame
+    public void Dash()
+    {
+        if (!dashInCooldown)
+        {
+            StartCoroutine(Dashing());
+        }
+    }
+
+    private void Update ()
+    {
+        dashCurrentCooldown -= Time.deltaTime;
+
+        if (dashCurrentCooldown <= 0)
+        {
+            dashInCooldown = false;
+        }
+    }
+
+    IEnumerator Dashing() //Dash shoud make you advance a certain distance, not just boost your Speed !!
+    {
+        dashInCooldown = true;
+        dashCurrentCooldown = dashTotalCooldown;
+
+        playerController.SetCurrentSpeed(dashSpeed);
+
+        yield return new WaitForSeconds(dashDuration);
+
+        playerController.SetCurrentSpeed(playerController.GetBaseSpeed());
+    }
+
+}
---
+++ Unity Project V2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 currentInputedDirection;
+    private Rigidbody2D playerRigidbody2D;
+
+    [SerializeField]
+    private float baseSpeed; 
+    private float currentSpeed;
+
+    private DashManager dashManager;
+
+    private void Start()
+    {
+        currentSpeed = baseSpeed;
+
+        playerRigidbody2D = GetComponent<Rigidbody2D>();
+
+        dashManager = GetComponent<DashManager>();
+    }
+
+
+    private void Update()
+    {
+        //Take input
+        currentInputedDirection = Vector2.right * Input.GetAxisRaw("Horizontal") + Vector2.up * Input.GetAxisRaw("Vertical");
+        currentInputedDirection.Normalize();
+
+        if (Input.GetAxis("Jump") > 0)
+        {
+            dashManager.Dash();
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        //Move player's Rigidbody2D
+        playerRigidbody2D.velocity = currentInputedDirection * currentSpeed; //Is " * Time.fixedDeltaTime" needed? !!
+    }
+
+    public float GetBaseSpeed()
+    {
+        return baseSpeed;
+    }
+
+    public float GetCurrentSpeed() //May me unnecessary !!
+    {
+        return currentSpeed;
+    }
+
+    public void SetCurrentSpeed(float currentSpeed)
+    {
+        this.currentSpeed = currentSpeed;
+    }
+}
